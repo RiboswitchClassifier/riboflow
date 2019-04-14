273906b21abe395d2b57ea938c156c3e47bc36f1
Keshav Aditya R.P <keshavaditya26896@gmail.com>
2019-04-14 17:02:45 +0530

    Bug Fix3 : In progress : Version 0.1dev for Riboflow

--- setup.py
+++ setup.py
@@ -14,9 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     url="https://github.com/KeshavAdityaRP/riboflow",
     packages=find_packages('riboflow'),
-    package_data = {
-      'static': ['*']
-    },
+    include_package_data = True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
